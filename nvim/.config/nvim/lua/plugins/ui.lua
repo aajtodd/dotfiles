90ad9509a7783a5eab9b264964928a6fc1ebd9ab
Aaron J Todd <todaaron@amazon.com>
2024-03-13 12:12:57 -0400

    fix gitsigns

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -29,7 +29,10 @@ return {
     },
 
     {
-        "lewis6991/gitsigns.nvim"
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require('gitsigns').setup()
+        end
     },
 
     -- FIXME - work on improving this
