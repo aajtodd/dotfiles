65e8ebddda35e078cf612d98dfe665cab61debd6
Aaron J Todd <todaaron@amazon.com>
2024-03-13 12:10:23 -0400

    add iterm mappings

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -18,5 +18,13 @@ config.font_size = 12
 
 config.color_scheme = 'Ocean (dark) (terminal.sexy)'
 
+-- Custom keybindings
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
+
 -- and finally, return the configuration to wezterm
 return config
