4bbdc1ddd574222d764b5ae13434bec5f98a16df
Aaron J Todd <todaaron@amazon.com>
2024-03-14 14:23:12 -0400

    add hidden files

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -1,4 +1,7 @@
 local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+local telescopeConfig = require("telescope.config")
+
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'telescope: [f]ind [f]iles' })
 vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'telescope: find files' })
@@ -17,4 +20,28 @@ vim.api.nvim_create_autocmd({"vimenter"}, {
     end
 })
 
-require('telescope').setup()
+-- 
+-- Add hidden files to default search
+-- 
+
+-- Clone the default Telescope configuration
+local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+-- I want to search in hidden/dot files.
+table.insert(vimgrep_arguments, "--hidden")
+-- I don't want to search in the `.git` directory.
+table.insert(vimgrep_arguments, "--glob")
+table.insert(vimgrep_arguments, "!**/.git/*")
+
+telescope.setup({
+	defaults = {
+		-- `hidden = true` is not supported in text grep commands.
+		vimgrep_arguments = vimgrep_arguments,
+	},
+	pickers = {
+		find_files = {
+			-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+		},
+	},
+})
